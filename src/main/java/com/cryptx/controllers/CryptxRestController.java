20bb85e39694abebd61c0b18c7094fc45a8cde34
pranavchauthaiwale <pranavchauthaiwale@gmail.com>
2017-11-25 05:07:23 -0500

    skeleton for adding bank details of user

--- src/main/java/com/cryptx/controllers/CryptxRestController.java
+++ src/main/java/com/cryptx/controllers/CryptxRestController.java
@@ -18,27 +18,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class CryptxRestController {
 
 	private static final Logger logger = LoggerFactory.getLogger(CryptxRestController.class);
-	
-	@RequestMapping(value="in", method=RequestMethod.GET)
+
+	@RequestMapping(value = "in", method = RequestMethod.GET)
 	public Map<String, String> loginUser() {
 		logger.info("Logging user In");
 		Map<String, String> resource = new HashMap<String, String>();
-        resource.put("resource", "Login user Resource");
-        return resource;
+		resource.put("resource", "Login user Resource");
+		return resource;
 	}
-	
+
 	@RequestMapping(value = "out", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void logout(HttpSession session) {
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void logout(HttpSession session) {
 		logger.info("Loggin-out User. Invalidating Session");
-        session.invalidate();
-    }
-	
-	@RequestMapping(value="register", method=RequestMethod.POST)
+		session.invalidate();
+	}
+
+	@RequestMapping(value = "register", method = RequestMethod.POST)
 	public Map<String, String> signup() {
 		logger.info("Registering new user in cryptx");
 		Map<String, String> resource = new HashMap<String, String>();
-        resource.put("resource", "Register User Resource");
-        return resource;
+		resource.put("resource", "Register User Resource");
+		return resource;
+	}
+
+	@RequestMapping(value = "getbankdetails", method = RequestMethod.GET)
+	public Map<String, String> getBankDetails() {
+		logger.info("Request for bank details of user");
+		Map<String, String> resource = new HashMap<String, String>();
+		resource.put("resource", "Get Bank Details Resource");
+		return resource;
+	}
+
+	@RequestMapping(value = "addbankdetails", method = RequestMethod.POST)
+	public Map<String, String> addBankDetails() {
+		logger.info("Request for adding bank details for user");
+		Map<String, String> resource = new HashMap<String, String>();
+		resource.put("resource", "Add bank Details Resource");
+		return resource;
+	}
+
+	@RequestMapping(value = "register", method = RequestMethod.POST)
+	public Map<String, String> updateBankDetails() {
+		logger.info("Registering for updating bank details of user");
+		Map<String, String> resource = new HashMap<String, String>();
+		resource.put("resource", "Resource");
+		return resource;
 	}
 }
