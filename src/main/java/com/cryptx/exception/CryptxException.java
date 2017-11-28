9a90a0eb3ba58bc7f92a5f30d81a4dbaed9955be
pranavchauthaiwale <pranavchauthaiwale@gmail.com>
2017-11-28 15:44:32 -0500

    Exception type for application

---
+++ src/main/java/com/cryptx/exception/CryptxException.java
@@ -0,0 +1,13 @@
+package com.cryptx.exception;
+
+public class CryptxException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4493106941824868653L;
+
+	public CryptxException(String message) {
+		super(message);
+	}
+}
