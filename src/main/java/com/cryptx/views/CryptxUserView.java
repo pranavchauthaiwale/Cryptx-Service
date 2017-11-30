518996217b0d897618ae4d55d791970f36777395
pranavchauthaiwale <pranavchauthaiwale@gmail.com>
2017-11-29 19:20:45 -0500

    User View Class for accepting user registration requests

---
+++ src/main/java/com/cryptx/views/CryptxUserView.java
@@ -0,0 +1,16 @@
+package com.cryptx.views;
+
+import com.cryptx.models.CryptxUser;
+
+public class CryptxUserView {
+	private CryptxUser user;
+
+	public CryptxUser getUser() {
+		return user;
+	}
+
+	public void setUser(CryptxUser user) {
+		this.user = user;
+	}
+	
+}
