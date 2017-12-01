c050eefe025248e423d48fe2a598b7290dfa1ab6
pranavchauthaiwale <pranavchauthaiwale@gmail.com>
2017-12-01 00:38:31 -0500

    Changes in method declaration in IDataAccess interface

--- src/main/java/com/cryptx/dao/IDataAccess.java
+++ src/main/java/com/cryptx/dao/IDataAccess.java
@@ -1,10 +1,11 @@
 package com.cryptx.dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public interface IDataAccess {
 	
 	public static final String DATA_ACCESS_SERVICE = "DataAccessService";
-	ResultSet executeQuery(String queryString);
+	ResultSet executeQuery(String queryString) throws SQLException;
 
 }
