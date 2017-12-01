de9e0f2b1cca71d67d80c787f9ba549ad2b73e62
pranavchauthaiwale <pranavchauthaiwale@gmail.com>
2017-12-01 00:37:15 -0500

    executeQuery method throws SQLException

--- src/main/java/com/cryptx/dao/DataAccess.java
+++ src/main/java/com/cryptx/dao/DataAccess.java
@@ -5,6 +5,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.cryptx.utils.ConnectionManager;
@@ -12,21 +14,16 @@ import com.cryptx.utils.ConnectionManager;
 @Service(IDataAccess.DATA_ACCESS_SERVICE)
 public class DataAccess implements IDataAccess {
 
+	private static final Logger logger = LoggerFactory.getLogger(DataAccess.class);
+
 	private static Connection connection = ConnectionManager.getConnection();
 
 	@Override
-	public ResultSet executeQuery(String queryString) {
+	public ResultSet executeQuery(String queryString) throws SQLException {
 		ResultSet rs = null;
-		try {
-			Statement stmt = connection.createStatement();
-			rs = stmt.executeQuery(queryString);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+		Statement stmt = connection.createStatement();
+		logger.info(String.format("Executing Query: [%s]", queryString));
+		rs = stmt.executeQuery(queryString);
 		return rs;
 	}
 }
